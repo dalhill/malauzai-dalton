0d6fe915b82d5eb1a3d03cc20e9b695d21c5b9ad
Dalton Hill <dalton.hill2022@gmail.com>
2018-07-24 00:10:10 -0500

    Fixed issues with yml file. API now responds with client data if it exists.

--- index.js
+++ index.js
@@ -1,9 +1,39 @@
 const serverless = require("serverless-http");
+const bodyParser = require("body-parser");
 const express = require("express");
-
+const AWS = require("aws-sdk");
 const app = express();
-app.get("/", (req, res) => {
-  res.send("Hello World!")
+
+
+const CUSTOMERS_TABLE = process.env.CUSTOMERS_TABLE;
+const dynamoDB = new AWS.DynamoDB.DocumentClient({region: 'us-east-1'});
+
+app.use(bodyParser.json({ strict: false }));
+
+app.post("/", (req, res) => {
+  if (typeof req.body.customerName !== "string") {
+    res.status(400).json({ error: "Please specify a customerName of type string."});
+    return;
+  }
+
+  const params = {
+    TableName: CUSTOMERS_TABLE,
+    Key: {
+      customerName: req.body.customerName
+    }
+  };
+
+  dynamoDB.get(params, (error, result) => {
+    if (error) {
+      console.log(error);
+      res.status(400).json({ error: "Could not get customer." });
+    } else if (result && result.Item) {
+      const customer = result.Item;
+      res.json({ customer });
+    } else {
+      res.status(404).json({ error: "Customer not found." })
+    }
+  })
 });
 
 module.exports.handler = serverless(app);
