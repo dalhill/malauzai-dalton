777653e8f4e66fff1c38afc6add0d689a6e2a523
Dalton Hill <dalton.hill2022@gmail.com>
2018-07-24 10:47:25 -0500

    updated yml to specify port. updated index to use utils file.

--- index.js
+++ index.js
@@ -1,12 +1,8 @@
 const serverless = require("serverless-http");
 const bodyParser = require("body-parser");
 const express = require("express");
-const AWS = require("aws-sdk");
 const app = express();
-
-
-const CUSTOMERS_TABLE = process.env.CUSTOMERS_TABLE;
-const dynamoDB = new AWS.DynamoDB.DocumentClient({region: 'us-east-1'});
+const { getCustomer, createRequest } = require("./customers/utils");
 
 app.use(bodyParser.json({ strict: false }));
 
@@ -14,26 +10,42 @@ app.post("/", (req, res) => {
   if (typeof req.body.customerName !== "string") {
     res.status(400).json({ error: "Please specify a customerName of type string."});
     return;
+  } else if (typeof req.body.latitude !== "number") {
+    res.status(400).json({ error: "Please specify a latitude of type number."});
+    return;
+  } else if (typeof req.body.longitude !== "number") {
+    res.status(400).json({ error: "Please specify a latitude of type number."});
+    return;
   }
 
-  const params = {
-    TableName: CUSTOMERS_TABLE,
-    Key: {
-      customerName: req.body.customerName
-    }
-  };
-
-  dynamoDB.get(params, (error, result) => {
+  getCustomer(req.body.customerName, (error, customer) => {
     if (error) {
-      console.log(error);
-      res.status(400).json({ error: "Could not get customer." });
-    } else if (result && result.Item) {
-      const customer = result.Item;
-      res.json({ customer });
+      res.status(error.status).json({ error: error.message });
     } else {
-      res.status(404).json({ error: "Customer not found." })
+      res.status(200).json({ customer });
     }
   })
+
+
+
+  // const params = {
+  //   TableName: CUSTOMERS_TABLE,
+  //   Key: {
+  //     customerName: req.body.customerName
+  //   }
+  // };
+//
+  // dynamoDB.get(params, (error, result) => {
+  //   if (error) {
+  //     console.log(error);
+  //     res.status(400).json({ error: "Could not get customer." });
+  //   } else if (result && result.Item) {
+  //     const customer = result.Item;
+  //     res.json({ customer });
+  //   } else {
+  //     res.status(404).json({ error: "Customer not found." })
+  //   }
+  // })
 });
 
 module.exports.handler = serverless(app);
